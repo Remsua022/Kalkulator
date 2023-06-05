8b18b1280bbfe4345a056c6788c8292948966733
Remsua022 <remek.stachacz@icloud.com>
2023-06-05 19:43:14 +0200

    initial commit

---
+++ src/main/java/org/example/Kalkulator.java
@@ -0,0 +1,17 @@
+package org.example;
+
+public class Kalkulator {
+
+    public int add(int x, int y) {
+        int z;
+        int pierwsza = x;
+        int druga = y;
+        z = pierwsza + druga;
+        return z;
+    }
+
+
+}
+
+
+
